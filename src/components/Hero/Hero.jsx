f53d01dc786ff2c733c1aa3b8bdac414e57fc9cb
edwingpe <edwing.machado1@gmail.com>
2023-08-09 06:04:42 -0300

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
-    <div className='flex flex-col text-center md:flex-row md:justify-around '>
-        <div className='space-y-8 md:text-left'>
-            <span className='text-6xl font-sans font-extrabold md:text-7xl'>MYTINERARY</span>
+    <div className='flex flex-col text-center md:flex-row md:justify-center md: gap-16'>
+        <div className='space-y-8 lg:text-left'>
+            <span className='font-sans font-extrabold text-6xl md:text-8xl text-gray-700 bg-gradient-to-r from-red-500 to-purple-500 text-transparent bg-clip-text hover:cursor-pointer'>MYTINERARY</span>
             <p className='text-3xl'>Find your perfect trip, <br />designed by insiders who know <br /> and love their cities!</p>
             <button>
               <Link className='border-2 rounded-lg px-5 py-2 bg-orange-500 border-orange-600 text-center text-white font-bold' to={'/cities'}>
               Discover Us!</Link>
             </button>
         </div>
-        <div className='hidden md:block'>
-            aca fgotos
+        <div className='hidden lg:block'>
+            <img className= 'max-w-[80%]'src="https://www.pngmart.com/files/15/City-Building-Transparent-PNG.png" alt="" />
         </div>
     </div>
   )
